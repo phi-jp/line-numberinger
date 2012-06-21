a09988282c68b2edab469c103f848e763c835781
phi <phi1618jp@gmail.com>
2012-06-21 14:22:05 +0900

    add form.

--- main.js
+++ main.js
@@ -10,16 +10,66 @@ tm.main(function() {
     
     var eSrc = tm.dom.Element("#src");
     var eDst = tm.dom.Element("#dst");
+    var eLineNum = tm.dom.Element("#line-num");
+    var eDelimiter = tm.dom.Element("#delimiter");
     
-    eSrc.event.add("change", function() {
-        var lines = eSrc.value.split("\n");
-        eDst.value = "";
-        
-        for (var i=0,len=lines.length; i<len; ++i) {
-            var line = lines[i];
-            var index = (i+1).padding(3, '0');
-            line = index + "|" + line + "\n";
-            eDst.value += line;
-        }
-    });
+    eSrc.event.add("change", convert);
+    eLineNum.event.add("change", convert);
+    eDelimiter.event.add("change", convert);
+    
+    numbering();
 });
+
+var convert = function() {
+    var eModeList = document.mainForm.mode;
+    var mode = "add";
+    for (var i=0,len=eModeList.length; i<len; ++i) {
+        if (eModeList[i].checked === true) {
+            mode = eModeList[i].value;
+            break;
+        }
+    }
+    
+    if (mode == "add") {
+        numbering();
+    }
+    else {
+        remove();
+    }
+};
+
+var numbering = function() {
+    var eSrc = tm.dom.Element("#src");
+    var eDst = tm.dom.Element("#dst");
+    var eLineNum = tm.dom.Element("#line-num");
+    var eDelimiter = tm.dom.Element("#delimiter");
+    
+    var offset = Number(eLineNum.value);
+    var delimiter = eDelimiter.value;
+    
+    var lines = eSrc.value.split("\n");
+    var destLines = [];
+    
+    for (var i=0,len=lines.length; i<len; ++i) {
+        var line = lines[i];
+        var index = (i + offset).padding(3, '0');
+        line = index + delimiter + line;
+        destLines.push(line);
+    }
+    eDst.value = destLines.join('\n');
+};
+
+var remove = function() {
+    var eSrc = tm.dom.Element("#src");
+    var eDst = tm.dom.Element("#dst");
+    var lines = eSrc.value.split("\n");
+    var destLines = [];
+    
+    for (var i=0,len=lines.length; i<len; ++i) {
+        var line = lines[i];
+        line = line.substr(4);
+        destLines.push(line);
+    }
+    eDst.value = destLines.join('\n');
+};
+
